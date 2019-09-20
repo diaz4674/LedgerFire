d0b0374ad4c4d62fc42c8499a7f15b94a6204b70
Miguel Diaz <diaz4674@gmail.com>
2019-09-19 20:24:02 -0700

    added middleware to check returned data

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,10 +3,35 @@ import BankTypesTable from "./BankTypesTable";
 
 const Landing = props => {
   const [data, setData] = React.useState([]);
+  const [activate, setactivate] = React.useState(0);
 
-  const updateData = newData => {
-    setData(data => [...data, newData]);
+  let add = () => {
+    setactivate(1);
   };
+  const updateData = async newData => {
+    let check = null;
+
+    if (data.length > 0) {
+      await data.map(same => {
+        same.name === newData.name ? (check = true) : (check = false);
+
+        if (same.name !== newData.name) {
+          add();
+          activate === 1 ? setData(data => [...data, newData]) : null;
+          activate(0);
+        } else if (same.name === newData.name) {
+          console.log("same");
+          //  data.indexOf(function(name) {
+          //   console.log("index");
+          // });
+          // data.map(thisData => thisData.find(o => o.id === newData.id) || thisData);
+        }
+      });
+    } else {
+      setData(data => [...data, newData]);
+    }
+  };
+
   console.log(data);
   return (
     <>

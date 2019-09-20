20cd1570ec7d7c7a540f7405f801c72e72a0886c
Miguel Diaz <diaz4674@gmail.com>
2019-09-19 20:58:41 -0700

    Successully updates new array objects w/out duplicating them

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,30 +3,15 @@ import BankTypesTable from "./BankTypesTable";
 
 const Landing = props => {
   const [data, setData] = React.useState([]);
-  const [activate, setactivate] = React.useState(1);
 
-  let add = () => {
-    setactivate(1);
-  };
   const updateData = async newData => {
-    let check = null;
-
     if (data.length > 0) {
-      await data.map(same => {
-        same.name === newData.name ? (check = true) : (check = false);
-
-        if (same.name !== newData.name) {
-          add();
-          activate === 1 ? setData(data => [...data, newData]) : null;
-          activate(0);
-        } else if (same.name === newData.name) {
-          console.log("same");
-          //  data.indexOf(function(name) {
-          //   console.log("index");
-          // });
-          // data.map(thisData => thisData.find(o => o.id === newData.id) || thisData);
-        }
-      });
+      if (data.some(o => o.name === newData.name)) {
+        data.find(o => o.name === newData.name).expense = newData.expense;
+        console.log(data);
+      } else {
+        setData(data => [...data, newData]);
+      }
     } else {
       setData(data => [...data, newData]);
     }
